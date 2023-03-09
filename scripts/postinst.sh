1c4dbc7f37a85d0388e68dfb08baad9ead7d0236
tang <tanguy.bonneau@gmail.com>
2023-03-09 19:36:57 +0100

    Bump psutil python lib

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install libs
-python3 -m pip install --trusted-host pypi.org "psutil==5.9.0"
+python3 -m pip install --trusted-host pypi.org "psutil==5.9.4"
 if [ $? -ne 0 ]; then
     exit 1
 fi
