7538f870c715c72bfd04b51ae839d89ca61f329f
tang <tanguy.bonneau@gmail.com>
2023-11-14 15:16:15 +0100

    Use Cleep config-base-editor

--- frontend/config-code.component.js
+++ frontend/config-code.component.js
@@ -20,22 +20,9 @@
  */
 angular.module('Cleep').component('configCode', {
     template: `
-    <md-content layout-fill>
-        <md-toolbar class="md-hue-3">
-            <div class="md-toolbar-tools">
-                <h2 flex md-truncate>{{ $ctrl.clTitle }}</h2>
-                <md-button
-                    ng-repeat="btn in $ctrl.btns"
-                    ng-class="btn.style"
-                    ng-click="$ctrl.onClick($event, btn)"
-                >
-                    <cl-icon ng-if="btn.icon" class="icon-white" cl-mdi="{{ btn.icon }}"></cl-icon>
-                    {{ btn.label }}
-                </md-button>
-            </div>
-        </md-toolbar>
+    <config-base-viewer cl-buttons="$ctrl.clButtons" cl-title="$ctrl.clTitle">
         <div ui-codemirror ui-codemirror-opts="$ctrl.config" ng-model="$ctrl.clModel" class="codemirror-code">
-    </md-content>
+    </config-base-viewer>
     `,
     bindings: {
         clTitle: '@?',
@@ -45,14 +32,10 @@ angular.module('Cleep').component('configCode', {
     },
     controller: function ($rootScope, cleepService, rpcService, confirmService, toastService) {
         const ctrl = this;
-        ctrl.btns = [];
         ctrl.config = {};
         ctrl.cmInstance = null;
 
         ctrl.$onChanges = function (changes) {
-            if (changes.clButtons?.currentValue) {
-                ctrl.prepareButtons(changes.clButtons.currentValue);
-            }
             if (changes.clConfig?.currentValue) {
                 ctrl.prepareConfig(changes.clConfig.currentValue);
             }
@@ -70,26 +53,11 @@ angular.module('Cleep').component('configCode', {
                     cmInstance.focus();
                     (customOnLoad || angular.noop)(cmInstance);
                 },
-            }
-        };
-
-        ctrl.prepareButtons = function (buttons) {
-            for (const btn of buttons) {
-                ctrl.btns.push({
-                    label: btn.label ?? '',
-                    style: btn.style ?? 'md-primary md-raised',
-                    icon: btn.icon,
-                    click: btn.click,
-                });
-            }
+            };
         };
 
         ctrl.refreshEditor = function () {
             ctrl.cmInstance?.refresh();
-        };
-
-        ctrl.onClick = ($event, button) => {
-            (button.click || angular.noop)(button.meta);
         };
     },
 });
